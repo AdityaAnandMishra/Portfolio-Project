f3af9e32680caaa9d7e31f84e6611dd509ceac6e
AdityaAnandMishra <aamishra.075@gmail.com>
2023-09-05 09:43:24 +0530

    Minor changes

--- DEVCONNECTOR/routes/api/profile.js
+++ DEVCONNECTOR/routes/api/profile.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/Users');
+const Profile = require('../../models/Profiles');
+const { check, validationResult } = require('express-validator');
 
-// route  get api/profiles
-// desc   test route
-// access public
+// route  get api/profile/me
+// desc   get current user profile
+// access private
 
-router.get('/' , (req, res) => res.send('users profiles')) ;
+router.get('/me' , auth, async (req, res) => {
+    try {
+        //to get the user profile
+       const profile= await Profile.findOne({ user: req.user.id }).populate( 'user', ['name', 'avatar']);
+
+       //to check if user exsist
+       if(!profile){
+        return res.status(500).json({ msg: 'There is no profile for the User'})
+       }
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+});
+
+// route  get api/profile
+// desc   create or update User Profile
+// access Private
 
 module.exports = router;
